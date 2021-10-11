0c0ccbee56bf907e38fe359d3f9f98fe48ba1aaa
Mehmet Yaman <mehmetyaman331@gmail.com>
2021-10-11 14:35:45 +0300

    herosection slider

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
+import Slider from "react-slick";
 
- const HeroSection = () => {
-    return (
+const HeroSection = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    arrows: false,
+    slideToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="relative h-[500px] before:bg-gradient-to-r before:from-primary-brand-color before:to-transparent before:absolute before:inset-0 before:w-full before:h-full before:z-10">
+      <Slider {...settings}>
         <div>
-            hero section
+          <img
+            className="w-full h-[500px] object-cover"
+            src="https://getir.com/_next/static/images/getir-mainpage-4-1751ad2d8fb42a88742d6751938da7e7.jpg"
+            alt="resim"
+          />
         </div>
-    )
-}
+
+        <div>
+          <img
+            className="w-full h-[500px] object-cover"
+            src="https://getir.com/_next/static/images/getir-mainpage-1-757eca6a46304def60cabce74d3f20a2.jpg"
+            alt="resim"
+          />
+        </div>
+      </Slider>
+      <div className="container flex justify-between items-center absolute top-0 left-1/2 -translate-x-1/2 h-full z-20">
+        <div>
+          <img
+            src="https://getir.com/_next/static/images/bimutluluk-b3a7fcb14fc9a9c09b60d7dc9b1b8fd6.svg"
+            alt="logo"
+          />
+          <h3 className="text-4xl mt-8  font-semibold text-white">
+            Dakikalar içinde <br /> kapınızda
+          </h3>
+        </div>
+        <div className="w-[400px] rounded-lg bg-gray-50 p-6">
+            <h4 className="text-primary-brand-color text-center font-semibold">Giriş yap veya kayıt ol</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default HeroSection;
