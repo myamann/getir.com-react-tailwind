c951da4f4994e034bd7054e343756a0db89e8973
Mehmet Yaman <mehmetyaman331@gmail.com>
2021-10-11 17:30:11 +0300

    category component

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import categoryData from 'api/categories.json';
+import Category from 'components/ui/Category';
+
 
  const Categories = () => {
+
+    const [categories,setCategories] = useState([]);
+
+    useEffect(() => {
+        // istek
+        // setTimeout(()=> setCategories(categoryData),2000);
+        setCategories(categoryData);
+    }, [])
+
     return (
-        <div>
-            categories
+        <div className="bg-white py-4">
+            <div className="container mx-auto">
+                <h3 className="text-sm font-semibold mb-3">Kategoriler</h3>
+                <div className="grid grid-cols-10">
+                    {!categories.length && 'Yükleniyor..'}
+                    {categories && categories.map((category,index) => <Category key={index} category={category}/> )}
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -23,7 +23,6 @@ export default function HeroSection() {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        speed: 500,
         autoplaySpeed: 3500,
         cssEase: "linear"
       };
@@ -32,15 +31,15 @@ export default function HeroSection() {
         <div className="relative h-[500px] before:bg-gradient-to-r before:from-primary-brand-color before:to-transparent before:absolute before:inset-0 before:w-full before:h-full before:z-10">
            <Slider {...settings}>
                 <div>
-                    <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-4-1751ad2d8fb42a88742d6751938da7e7.jpg" />
+                    <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-4-1751ad2d8fb42a88742d6751938da7e7.jpg" alt="slider1"/>
                 </div>
                 <div>
-                    <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-1-757eca6a46304def60cabce74d3f20a2.jpg" />
+                    <img className="w-full h-[500px] object-cover" src="https://getir.com/_next/static/images/getir-mainpage-1-757eca6a46304def60cabce74d3f20a2.jpg" alt="slider2"/>
                 </div>
             </Slider>
             <div className="container flex justify-between items-center absolute top-0 left-1/2 -translate-x-1/2 h-full z-20">
                 <div>
-                    <img src="https://getir.com/_next/static/images/bimutluluk-b3a7fcb14fc9a9c09b60d7dc9b1b8fd6.svg" />
+                    <img src="https://getir.com/_next/static/images/bimutluluk-b3a7fcb14fc9a9c09b60d7dc9b1b8fd6.svg" alt="logo1"/>
                     <h3 className="text-4xl mt-8 font-semibold text-white">
                         Dakikalar içinde <br /> kapınızda <br />
                     </h3>
