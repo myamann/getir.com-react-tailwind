490c51b8aeee05c48eb7f782220f4cf7ae308351
Mehmet Yaman <mehmetyaman331@gmail.com>
2021-10-12 11:16:05 +0300

    campaigns finish

--- src/components/Campaigns.js
+++ src/components/Campaigns.js
@@ -2,6 +2,23 @@ import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 import Banners from "api/banners.json";
 import Title from "./ui/Title";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+
+function NextBtn({  onClick }) {
+  return (
+    <button className={`text-brand-color absolute top-1/2 -right-6 -translate-y-1/2`}  onClick={onClick}>
+      <IoIosArrowForward size={22} />
+    </button>
+  );
+}
+
+function PrevBtn({ onClick }) {
+  return (
+    <button className={`text-brand-color absolute top-1/2 -left-6 -translate-y-1/2`}   onClick={onClick}>
+      <IoIosArrowBack size={22}/>
+    </button>
+  );
+}
 
 const Campaigns = () => {
   const [banners, setBanners] = useState([]);
@@ -14,12 +31,14 @@ const Campaigns = () => {
     dots: false,
     infinite: true,
     speed: 500,
-    arrows: false,
+    arrows: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3500,
     cssEase: "linear",
+    nextArrow: <NextBtn />,
+    prevArrow: <PrevBtn />,
   };
 
   return (
