fed450f709e9b8c41c8f538132e4359db6e05f0f
Mehmet Yaman <mehmetyaman331@gmail.com>
2021-10-13 11:27:25 +0300

    campaigns responsive

--- src/components/Campaigns.js
+++ src/components/Campaigns.js
@@ -4,9 +4,12 @@ import Banners from "api/banners.json";
 import Title from "./ui/Title";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
-function NextBtn({  onClick }) {
+function NextBtn({ onClick }) {
   return (
-    <button className={`text-brand-color absolute top-1/2 -right-6 -translate-y-1/2`}  onClick={onClick}>
+    <button
+      className={`text-brand-color absolute top-1/2 -right-6 -translate-y-1/2`}
+      onClick={onClick}
+    >
       <IoIosArrowForward size={22} />
     </button>
   );
@@ -14,8 +17,11 @@ function NextBtn({  onClick }) {
 
 function PrevBtn({ onClick }) {
   return (
-    <button className={`text-brand-color absolute top-1/2 -left-6 -translate-y-1/2`}   onClick={onClick}>
-      <IoIosArrowBack size={22}/>
+    <button
+      className={`text-brand-color absolute top-1/2 -left-6 -translate-y-1/2`}
+      onClick={onClick}
+    >
+      <IoIosArrowBack size={22} />
     </button>
   );
 }
@@ -32,13 +38,37 @@ const Campaigns = () => {
     infinite: true,
     speed: 500,
     arrows: true,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3500,
     cssEase: "linear",
+    
     nextArrow: <NextBtn />,
     prevArrow: <PrevBtn />,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          arrows:false
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          arrows:false
+        },
+      },
+    ],
   };
 
   return (
